8ecbf49f16ea12f6950959a40927732229d49e6b
Pippin Williamson <pippin@pippinsplugins.com>
2015-02-20 11:20:39 -0600

    remove analytics

--- pippinsplugins-custom.php
+++ pippinsplugins-custom.php
@@ -235,24 +235,6 @@ add_filter( 'upload_mimes', 'pw_allowed_mime_types' );
 remove_action( 'plugins_loaded', array( 'EDD_Heartbeat', 'init' ) );
 
 
-
-function pw_analytics() {
-?>
-<script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-  ga('create', 'UA-23172567-1', 'pippinsplugins.com');
-  ga('send', 'pageview');
-
-</script>
-<?php
-}
-add_action( 'wp_footer', 'pw_analytics' );
-
-
 // We need to register the EDD post types now b/c they
 // are registered on the init hook in EDD core.
 // The EDD post types needs to be registered now 
